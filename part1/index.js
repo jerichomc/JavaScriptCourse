2dea08be9700917b574fb41f985e202b6a06d16d
jerichomc <jerichomc255@gmail.com>
2025-01-16 14:34:12 -0800

    added html template and some js

---
+++ part1/index.js
@@ -0,0 +1,7 @@
+console.log(`Hello`);
+
+window.alert(`This is an alert box`);
+window.alert(`This is an alert box 2`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById(`myP`).textContent = `paragraph element`;
