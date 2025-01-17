82df292827f5e90bcf3b8fac3f7aeb12482f5583
jerichomc <jerichomc255@gmail.com>
2025-01-16 16:09:18 -0800

    added some new contact along with js

--- part1/index.js
+++ part1/index.js
@@ -1,7 +1,20 @@
 console.log(`Hello`);
 
-window.alert(`This is an alert box`);
-window.alert(`This is an alert box 2`);
+// window.alert(`This is an alert box`);
+// window.alert(`This is an alert box 2`);
 
-document.getElementById("myH1").textContent = `Hello`;
-document.getElementById(`myP`).textContent = `paragraph element`;
+let fullName = "Jericho";
+let age = 24;
+let isStudent = true;
+let username;
+
+document.getElementById("p1").textContent = `My name is ${fullName}`;
+document.getElementById("p2").textContent = `I am ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("chosenUser").textContent = `Chosen Username: ${username}`
+    
+}
