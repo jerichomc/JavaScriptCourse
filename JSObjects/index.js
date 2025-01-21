3eab4a4938af9e9e4ae397556e9fe8ac3cd3917f
jerichomc <jerichomc255@gmail.com>
2025-01-21 11:30:26 -0800

    made an object tutorial page

---
+++ JSObjects/index.js
@@ -0,0 +1,36 @@
+// object - A collection of related properties and/or methods
+// Can represent real world objects
+// object = {key:value, function()}
+
+const person1 = {
+    firstName: "Kevin",
+    lastName: "James",
+    age: 33,
+    isEmployed: true,
+    sayHello: function(){
+        console.log("Hello")
+    },
+}
+
+const person2 = {
+    firstName: "Adam",
+    lastName: "Sandler",
+    age: 31,
+    isEmployed: false,
+    sayHello: function(){
+        console.log("Hi")
+    }
+}
+
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.age);
+console.log(person1.isEmployed);
+
+console.log(person2.firstName);
+console.log(person2.lastName);
+console.log(person2.age);
+console.log(person2.isEmployed);
+
+person1.sayHello();
+person2.sayHello();
