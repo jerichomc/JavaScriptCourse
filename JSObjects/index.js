23f897ec5cbe50ff3ff8549c85ee6b82774adecf
jerichomc <jerichomc255@gmail.com>
2025-01-21 17:32:05 -0800

    constructor tutorial

--- JSObjects/index.js
+++ JSObjects/index.js
@@ -2,35 +2,43 @@
 // Can represent real world objects
 // object = {key:value, function()}
 
-const person1 = {
-    firstName: "Kevin",
-    lastName: "James",
-    age: 33,
-    isEmployed: true,
-    sayHello: function(){
-        console.log("Hello")
-    },
+// const person1 = {
+//     firstName: "Kevin",
+//     lastName: "James",
+//     age: 33,
+//     isEmployed: true,
+//     sayHello: function(){
+//         console.log("Hello")
+//     },
+// }
+
+// const person2 = {
+//     firstName: "Adam",
+//     lastName: "Sandler",
+//     age: 31,
+//     isEmployed: false,
+//     sayHello: function(){
+//         console.log("Hi")
+//     }
+// }
+
+// console.log(person1.firstName);
+// console.log(person1.lastName);
+// console.log(person1.age);
+// console.log(person1.isEmployed);
+
+// console.log(person2.firstName);
+// console.log(person2.lastName);
+// console.log(person2.age);
+// console.log(person2.isEmployed);
+
+// person1.sayHello();
+// person2.sayHello();
+
+//constructors 
+function Car(make, model, year, color){
+    this.make = make,
+    this.model = model,
+    this.year = year,
+    this.color = color,
 }
-
-const person2 = {
-    firstName: "Adam",
-    lastName: "Sandler",
-    age: 31,
-    isEmployed: false,
-    sayHello: function(){
-        console.log("Hi")
-    }
-}
-
-console.log(person1.firstName);
-console.log(person1.lastName);
-console.log(person1.age);
-console.log(person1.isEmployed);
-
-console.log(person2.firstName);
-console.log(person2.lastName);
-console.log(person2.age);
-console.log(person2.isEmployed);
-
-person1.sayHello();
-person2.sayHello();
